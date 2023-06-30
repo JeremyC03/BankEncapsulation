3f333f93ef82bddfe6deb122ab3dc7eea0a15844
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-06-30 14:32:33 -0400

    Finished Exercise 1

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance;
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set 
+            { 
+                _balance = 0; 
+            }
+        }
+        
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myAccount = new BankAccount();
+
+            Console.WriteLine("Please enter the amount you wish to deposit.");
+            myAccount.Deposit(100);
+            Console.WriteLine("You deposited $100.");
+            double currentBalance = myAccount.GetBalance();
+            Console.WriteLine($"Current balance in account is: ${currentBalance}.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter the amount you wish to withdraw.");
+            myAccount.Withdraw(25);
+            Console.WriteLine("You withdrew $25.");
+            double newCurrentBalance = myAccount.GetBalance();
+            Console.WriteLine($"Current balance in account is: ${newCurrentBalance}.");
+
         }
     }
 }
